5ec9f2f47773ec648d52d11eeaa4e581f1a048ca
alizamir25 <ali0786zamir@gmail.com>
2024-08-20 13:27:41 +0100

    Patch

--- app.js
+++ app.js
@@ -4,13 +4,11 @@ const computerScoreElem=document.querySelector('.scorebox h2:nth-of-type(1)');
 const userScoreElem=document.querySelector('.scorebox h2:nth-of-type(2)');
 let computerScore=0;
 let userScore=0;
-// Function to get a random choice for the computer
 const getComputerChoice=()=>{
     const choices = ['rock', 'paper', 'scissors'];
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
 };
-// Function to determine the winner of a round
 const determineWinner=(userChoice, computerChoice)=>{
     if (userChoice===computerChoice) return 'tie';
     if (
@@ -22,12 +20,10 @@ const determineWinner=(userChoice, computerChoice)=>{
     }
     return 'computer';
 };
-// Function to update the scores
 const updateScores=()=>{
     computerScoreElem.textContent=`Computer Score: ${computerScore}`;
     userScoreElem.textContent=`User Score: ${userScore}`;
 };
-// Function to update the result display
 const updateResultDisplay=(result)=>{
     switch (result){
         case 'tie':
@@ -49,9 +45,7 @@ choices.forEach(choice=>{
         const userChoice=choice.classList[0]; // Get the user's choice
         const computerChoice=getComputerChoice(); // Get the computer's choice
         const result=determineWinner(userChoice, computerChoice);
-        // Update the result display
         updateResultDisplay(result);
-        // Update scores
         if (result==='user'){
             userScore++;
         } else if(result==='computer'){
